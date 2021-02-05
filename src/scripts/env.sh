2cd133b252d4a4beb359c2dbd7f44f07a327ea89
JingHengLin <405500538@gms.tku.edu.tw>
2021-02-05 15:28:22 +0800

    Add roscore to env.sh

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -3,3 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/../../devel/setup.bash
 My_Gazebo_Model_Path="$( cd "${DIR}/../models" && pwd )"
 export GAZEBO_MODEL_PATH=${My_Gazebo_Model_Path}
+
+roscore
