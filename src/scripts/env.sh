0937a8b56a0034fe16dc56b7b66a256ae935b52c
JingHengLin <405500538@gms.tku.edu.tw>
2021-03-29 11:57:14 +0800

    ROS_Control Go Straight by Force with Code without Balance Control with Script

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -5,11 +5,15 @@ My_Gazebo_Model_Path="$( cd "${DIR}/../models" && pwd )"
 export GAZEBO_MODEL_PATH=${My_Gazebo_Model_Path}
 
 gnome-terminal \
---window -e 'bash -c "cd ~/motorcycle_ws; roscore"' \
+--window -e 'bash -c "cd ~/motorcycle_ws; killall rosmaster; killall gzserver; killall gzclient; roscore"' \
 --tab -e 'bash -c "sleep 5; source devel/setup.bash; roslaunch motorcycle_gz motorcycle_world.launch"' \
---tab -e 'bash -c "sleep 5; source devel/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch"' \
---tab -e 'bash -c "sleep 5; source devel/setup.bash; roslaunch motorcycle_gz motorcycle.launch"'
+--tab -e 'bash -c "sleep 10; source devel/setup.bash; roslaunch motorcycle_gz motorcycle_force.launch"'
 
+# gnome-terminal \
+# --window -e 'bash -c "cd ~/motorcycle_ws; roscore"' \
+# --tab -e 'bash -c "sleep 5; source devel/setup.bash; roslaunch motorcycle_gz motorcycle_world.launch"' \
+# --tab -e 'bash -c "sleep 5; source devel/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch"' \
+# --tab -e 'bash -c "sleep 5; source devel/setup.bash; roslaunch motorcycle_gz motorcycle_force.launch"'
 
 # alias motorcycle_world_launch="source devel/setup.bash && roslaunch motorcycle_gz motorcycle_world.launch"
 # alias motorcycle_launch="source devel/setup.bash && roslaunch motorcycle_gz motorcycle.launch"
