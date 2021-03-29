324f80030dccdc9d4670052f5bdac236e0adacfd
JingHengLin <405500538@gms.tku.edu.tw>
2021-03-29 12:41:30 +0800

    ROS_Control Go Straight by Force with Code without Balance Control with Script V2

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/../../devel/setup.bash
-My_Gazebo_Model_Path="$( cd "${DIR}/../models" && pwd )"
-export GAZEBO_MODEL_PATH=${My_Gazebo_Model_Path}
+# My_Gazebo_Model_Path="$( cd "${DIR}/../models" && pwd )"
+# export GAZEBO_MODEL_PATH=${My_Gazebo_Model_Path}
 
 gnome-terminal \
 --window -e 'bash -c "cd ~/motorcycle_ws; killall rosmaster; killall gzserver; killall gzclient; roscore"' \
