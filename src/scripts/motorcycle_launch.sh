7bbed8413c1fe12aa1743b0a49153a09b9a1127f
JingHengLin <405500538@gms.tku.edu.tw>
2021-04-04 16:33:07 +0800

    delete useless script

--- src/scripts/motorcycle_launch.sh
+++
@@ -1,4 +0,0 @@
-source devel/setup.bash
-roslaunch motorcycle_gz motorcycle.launch
-# xdotool type 'source devel/setup.bash' && xdotool key Return
-# xdotool type 'roslaunch motorcycle_gz motorcycle.launch' && xdotool key Return
--- src/scripts/rosbridge_websocket_launch.sh
+++
@@ -1,2 +0,0 @@
-xdotool type 'source devel/setup.bash' && xdotool key Return
-xdotool type 'roslaunch rosbridge_server rosbridge_websocket.launch' && xdotool key Return
