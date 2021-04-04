7624627ee763eff1206954c5c3be00a7e4cc43c6
JingHengLin <405500538@gms.tku.edu.tw>
2021-04-04 20:12:30 +0800

    Gazebo Speed Control by Web, PD Controller for Bwheel, Show Feedback Velocity on web

--- src/scripts/env.sh
+++ src/scripts/env.sh
@@ -4,6 +4,7 @@ source ${DIR}/../../devel/setup.bash
 # My_Gazebo_Model_Path="$( cd "${DIR}/../models" && pwd )"
 # export GAZEBO_MODEL_PATH=${My_Gazebo_Model_Path}
 
+# (SpeedControlShowCurrentVelocityInterface.html)
 gnome-terminal \
 --window -e 'bash -c "cd ~/motorcycle_ws; killall rosmaster; killall gzserver; killall gzclient; roscore"' \
 --tab -e 'bash -c "sleep 1; source devel/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch"' \
@@ -13,7 +14,14 @@ gnome-terminal \
 
 
 # ------------------------------------------------Record------------------------------------------------------------------------------------------
-# # Gazebo Constant Speed Control by Web, PD Controller for Bwheel
+# # Gazebo Speed Control by Web, PD Controller for Bwheel, Show Feedback Velocity on web(SpeedControlShowCurrentVelocityInterface.html)
+# gnome-terminal \
+# --window -e 'bash -c "cd ~/motorcycle_ws; killall rosmaster; killall gzserver; killall gzclient; roscore"' \
+# --tab -e 'bash -c "sleep 1; source devel/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch"' \
+# --tab -e 'bash -c "sleep 1; source devel/setup.bash; roslaunch motorcycle_gz motorcycle_world.launch"' \
+# --tab -e 'bash -c "sleep 5; source devel/setup.bash; rosrun motorcycle_gz p_controller_WebVgoal"'
+
+# # Gazebo Speed Control by Web, PD Controller for Bwheel (SpeedControlInterface.html)
 # gnome-terminal \
 # --window -e 'bash -c "cd ~/motorcycle_ws; killall rosmaster; killall gzserver; killall gzclient; roscore"' \
 # --tab -e 'bash -c "sleep 1; source devel/setup.bash; roslaunch rosbridge_server rosbridge_websocket.launch"' \
